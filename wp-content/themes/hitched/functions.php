93788584f3724b0c8a5b50b2db7bab55d3d63419
carlos (el-cajon) <padi_05@yahoo.com>
2015-11-15 12:37:04 -0500

    Footer layout styles.

--- wp-content/themes/hitched/functions.php
+++ wp-content/themes/hitched/functions.php
@@ -8,6 +8,28 @@ function hitched_theme_setup() {
 add_action('after_setup_theme', 'hitched_theme_setup');
 
 function hitched_assets() {
-	wp_enqueue_style('twentyfifteen-style', get_stylesheet_uri());
+	wp_enqueue_style('hitched-css', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'hitched_assets');
+
+function remove_wp_open_sans() {
+	wp_deregister_style('open-sans');
+	wp_register_style('open-sans', false);
+}
+add_action('wp_enqueue_scripts', 'remove_wp_open_sans');
+
+add_filter('wp_nav_menu_args', function($args) {
+	if (!in_array($args['theme_location'], ['primary','secondary']) && isset($args['menu']) && $args['menu']->slug === 'social-media') {
+		$args['menu_class'] .= ' horizontal-list';
+		$args['link_before'] .= '<span class="no-text single-background">';
+		$args['link_after'] .= '</span>';
+	}
+	return $args;
+});
+
+add_filter('nav_menu_link_attributes', function($atts, $item, $args, $depth) {
+	if (strpos($atts['href'], $_SERVER['HTTP_HOST']) === FALSE) {
+		$atts['target'] = '_blank';
+	}
+	return $atts;
+}, 10, 4);

68cea5580fc3ae41809c806f3ed5a5ba9dc79e63
carlos (el-cajon) <padi_05@yahoo.com>
2015-11-15 22:12:12 -0500

    Styles for the maids page, same as bridal.

--- wp-content/themes/hitched/pages/bridal.php
+++ wp-content/themes/hitched/pages/bridal.php
@@ -1,7 +1,9 @@
 <?php $images = array_filter(get_attached_media('image'), function($img) { return !empty($img->post_excerpt); }); ?>
-<ul class="horizontal-list tc">
-	<?php foreach ($images as $img): ?>
-	<li><a class="no-underline no-color" target="_blank" href="<?php echo $img->post_excerpt; ?>"><img src="<?php echo $img->guid; ?>" alt=""><span><?php echo $img->post_title; ?></span></a></li>
-	<?php endforeach; ?>
-</ul>
-<a class="scroll-button uppercase no-underline no-color" href="#content">Back to top</a>
+<div class="designers-gallery">
+	<ul class="horizontal-list tc">
+		<?php foreach ($images as $img): ?>
+		<li><a class="no-underline no-color" target="_blank" href="<?php echo $img->post_excerpt; ?>"><img src="<?php echo $img->guid; ?>" alt=""><span><?php echo $img->post_title; ?></span></a></li>
+		<?php endforeach; ?>
+	</ul>
+	<a class="scroll-button uppercase no-underline no-color" href="#content">Back to top</a>
+</div>
